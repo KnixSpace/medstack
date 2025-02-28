95c510f34d55e5782236ce0a0d10d138ee14b4c9
Krupal Patel <krupalgp2003@gmail.com>
2025-02-28 13:18:34 +0530

    add role-based authorization check for editor users in authentication middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,3 +1,4 @@
+import { userRole } from "../constants/auth.js";
 import { readUser } from "../db/user.js";
 import { verifyJwt } from "../utils/jwt.js";
 import { verifyPassword } from "../utils/password.js";
@@ -29,7 +30,6 @@ export const isAuthenticated =
   async (ctx, next) => {
     const token = ctx.headers?.authorization?.split(" ")[1];
     const data = verifyJwt(token, process.env.JWT_PASSWORD_KEY);
-
     if (!data) {
       ctx.status = 401;
       ctx.body = { message: "unauthorized" };
@@ -46,8 +46,16 @@ export const isAuthenticated =
       { userId: data.userId },
       { projection: { password: 0 } }
     );
-
     if (!user) {
+      ctx.status = 401;
+      ctx.body = { message: "unauthorized" };
+      return;
+    }
+
+    if (
+      data.role === userRole.editor &&
+      !(await readUser({ userId: user.ownerDetails?.ownerId }))
+    ) {
       ctx.status = 401;
       ctx.body = { message: "unauthorized" };
       return;
